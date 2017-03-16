7db75991c93feae6285b31643c2d9eea6b0aaa5a
Pavel Korozevtsev <paxakor@yandex.ru>
2017-03-16 18:46:34 +0300

    tester improved (threads & code style)

--- code/tests/insatiable_client.cpp
+++ code/tests/insatiable_client.cpp
@@ -3,70 +3,113 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
+
 #include <netdb.h> 
 #include <netinet/in.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
+
+#include <algorithm>
+#include <functional>
+#include <iterator>
+#include <list>
+#include <thread>
 #include "stopwatch.hpp"
 
-int main(int argc, char** argv) {
-    enum { FILESZ = 4096 };
-    int portno, n;
-    struct sockaddr_in serv_addr;
-    struct hostent* server;
+enum { FILESZ = 4096 };
+using File = char[FILESZ];
 
-    if (argc < 3) {
-        fprintf(stderr, "usage: %s hostname port count\n", argv[0]);
-        return 1;
+void passert(bool val, const char* msg, bool use_perror = true) {
+    if (!val) {
+        if (use_perror) {
+            perror(msg);
+        } else {
+            fprintf(stderr, "insatiable_client's error: %s\n", msg);
+        }
+        exit(1);
     }
-    portno = atoi(argv[2]);
-    n = atoi(argv[3]);
-    server = gethostbyname(argv[1]);
+}
+
+const char* find_response_body(const File& buffer) {
+    static const char pattern[] = "\r\n\r\n";
+    return strlen(pattern) + std::search(buffer, buffer + strlen(buffer),
+        std::begin(pattern), std::begin(pattern) + strlen(pattern));
+}
+
+void send_request(const std::string* request, const File* file,
+                  const sockaddr_in* serv_addr) {
+    const auto sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    passert(sockfd >= 0, "can't open socket");
+    passert(connect(sockfd, reinterpret_cast<const sockaddr*>(serv_addr),
+                    sizeof(*serv_addr)) >= 0, "can't connect");
+    passert(write(sockfd, request->c_str(), request->size()) ==
+            static_cast<ssize_t>(request->size()), "can't write");
+    File buffer;
+    ssize_t sz = 0;
+    ssize_t len;
+    while ((len = read(sockfd, buffer + sz, FILESZ - sz)) > 0) {
+        sz += len;
+    }
+    close(sockfd);
+    buffer[sz] = 0;
+    const auto body = find_response_body(buffer);
+    passert(strncmp(*file, body, FILESZ) == 0, "files are not equal", false);
+}
+
+sockaddr_in make_serv_addr(const char* hostname, int portno) {
+    sockaddr_in serv_addr;
+    hostent* server = gethostbyname(hostname);
     if (!server) {
         fprintf(stderr, "ERROR, no such host\n");
-        return 1;
+        exit(1);
     }
     memset(&serv_addr, 0, sizeof(serv_addr));
     memcpy(&serv_addr.sin_addr.s_addr, server->h_addr, server->h_length);
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(portno);
+    return serv_addr;
+}
 
+void readfile(const char* fname, File& file) {
+    FILE *fp = fopen(fname, "r");
+    passert(fp, "can't open file");
+    size_t sz = 0;
+    size_t len;
+    while (sz + 1 < FILESZ && (len = fread(file + sz, 1, FILESZ - sz, fp))) {
+        passert(!ferror(fp), "can't read file");
+        sz += len;
+    }
+    fclose(fp);
+    file[sz] = 0;
+}
+
+void test(int sessions, const std::string* request, const File* file,
+          const sockaddr_in* serv_addr) {
+    for (int i = 0; i < sessions; ++i) {
+        send_request(request, file, serv_addr);
+    }
+}
+
+int main(int argc, char** argv) {
+    if (argc < 4) {
+        fprintf(stderr, "usage: %s hostname port file sessions threads\n", argv[0]);
+        return 1;
+    }
+    const auto request = "GET / HTTP/1.0\r\nHost: " + std::string(argv[1]) +
+        "\r\n\r\n";
+    const auto serv_addr = make_serv_addr(argv[1], atoi(argv[2]));
+    const auto sessions = atoi(argv[4]);
+    const auto thread_count = atoi(argv[5]);
+    File file;
+    readfile(argv[3], file);
     Stopwatch sw("DUDOS");
-    const std::string request = "GET / HTTP/1.0\r\nHost: " +
-        std::string(argv[1]) + "\r\n\r\n";
-    char file[FILESZ];
-    for (int i = 0; i < n; ++i) {
-        char buffer[FILESZ] = {0};
-        int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-        if (sockfd < 0) {
-            perror("ERROR opening socket");
-            return 1;
-        }
-        if (connect(sockfd, reinterpret_cast<struct sockaddr*>(&serv_addr),
-            sizeof(serv_addr)) < 0) {
-            perror("ERROR connecting");
-            return 1;
-        }
-        if (write(sockfd, request.c_str(), request.size()) < 0) {
-            perror("ERROR writing to socket");
-            return 1;
-        }
-        ssize_t sz = 0;
-        ssize_t len;
-        while ((len = read(sockfd, buffer + sz, sizeof(buffer) - sz)) > 0) {
-            sz += len;
-        }
-        buffer[sz] = 0;
-        close(sockfd);
-        if (i == 0) {
-            strncpy(file, buffer, FILESZ);
-        } else {
-            if (strncmp(file, buffer, FILESZ)) {
-                printf("insatiable_client's error: files are not equal\n");
-                return 1;
-            }
-        }
+    std::list<std::thread> threads;
+    for (int i = 0; i < thread_count; ++i) {
+        threads.emplace_back(test, sessions, &request, &file, &serv_addr);
+    }
+    for (auto& th : threads) {
+        th.join();
     }
     return 0;
 }

81bd695634c9b43df6a362da7e20fbec0d519db5
Pavel Korozevtsev <paxakor@yandex.ru>
2017-05-29 18:00:45 +0300

    test fixed

--- code/TODO.md
+++
@@ -1,5 +0,0 @@
-* Mime type
-* GET request: Range
-* error handling in read_config() is ugly
-* how to store cached files: list<array<char>>, where array has size of i/o buffer
-* sendfile
--- code/tests/proxy_test.py
+++ code/tests/proxy_test.py
@@ -6,14 +6,16 @@ from sys import argv
 from threading import Thread 
 import subprocess as sp
 
+errors = 0
+
+def join_url(host, page):
+    return host + page
+
 def join_page_name(host, page):
-    return host + '_' + page
+    return join_url(host, page).replace('/', '_')
 
 def join_page_name_proxy(host, page, thread_id):
     return join_page_name(host, page) + '_' + str(thread_id)
-
-def join_url(host, page):
-    return host + '/' + page
 
 def run_wget(args):
     args = ['wget'] + args
@@ -35,11 +37,25 @@ def for_all(f, xs, *args):
     for x in xs:
         f(x, *args)
 
+def equal_str(lines_a, lines_b, magic):
+    if len(lines_a) > len(lines_b):
+        lines_a, lines_b = lines_b, lines_a
+    fails = len(lines_b) - len(lines_a)
+    lines_b = lines_b[:-fails]
+    for la, lb in zip(lines_a, lines_b):
+        if la != lb:
+            fails += 1
+    return fails / len(lines_a) < magic
+
 def check(page, thread_id):
+    global errors
     fa = join_page_name(*page)
     fb = join_page_name_proxy(*page, thread_id)
-    if open(fa).read() != open(fb).read():
+    if not equal_str(open(fa).readlines(), open(fb).readlines(), 0.3):
         print(fa, '!=', fb)
+        errors += 1
+    else:
+        remove(fb)
 
 def thread_main(iterations, pages, thread_id):
     for _ in range(iterations):
@@ -55,17 +71,17 @@ def run_threads(pages, args):
     return threads
 
 def remove_files(pages, count):
-    for i in range(count):
-        for page in pages:
-            remove(join_page_name_proxy(*page, i))
     for page in pages:
         remove(join_page_name(*page))
 
 def main(args):
-    pages = [("eax.me", "autotools"),
-             ("eax.me", "llvm"),
-             ("eax.me", "lxc"),
-             ("eax.me", "gdb")]
+    global errors
+    pages = [
+                ("eax.me", "/llvm"),
+                ("ejudge.cs.hse.ru", "/"),
+                ("en.cppreference.com", "/w/cpp/utility/hash"),
+                ("www.consultant.ru", "/about"),
+            ]
     if len(args) < 3:
         print('Usage: {} port thread_count [iterations = 1]'.format(args[0]))
         return 1
@@ -74,7 +90,8 @@ def main(args):
     get_via_proxy.port = args[1]
     threads = run_threads(pages, args)
     for t in threads: t.join()
-    remove_files(pages, len(threads))
+    if errors == 0:
+        remove_files(pages, len(threads))
     return 0
 
 if __name__ == '__main__':
